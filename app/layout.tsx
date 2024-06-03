ae12bfe1fa2a0b79496f7d129971690f2abacf9c
Divyansh Vishwakarma <divyanshvishwakarma4@gmail.com>
2024-06-04 05:26:44 +0530

    meta data update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Versocia",
   description: "ddd",
+  icons: "/white-icon.png"
 };
 
 export default function RootLayout({
