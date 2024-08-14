657ff5ce9da4f283071333fe1b2542649a689eac
Divyansh Vishwakarma <119093182+deevee47@users.noreply.github.com>
2024-08-14 13:50:59 +0530

    content update

--- app/components/How.tsx
+++ app/components/How.tsx
@@ -18,13 +18,13 @@ const How = () => {
       />
       <div className="wrapper max-w-2xl mx-auto mt-20 bg-black ">
 
-        <Card title='Uncover Your Unique Story' description="Every remodeling company has a unique story to tell. We begin by delving deep into what sets your company apart—whether it's your innovative designs, exceptional craftsmanship, or outstanding customer service. We uncover the essence of your brand to effectively communicate your strengths to potential customers." />
+        <Card title='Uncover Your Unique Story' description="Every business has a unique story to tell. We begin by delving deep into what sets your company apart—whether it's your innovative solutions, exceptional service, or outstanding products. We uncover the essence of your brand to effectively communicate your strengths to potential customers." />
 
         <Card title='Create Captivating Visuals' description="We leverage our industry expertise to craft ad strategies that grab attention and inspire action. Using proven techniques, we create ads that stop the scroll, spark curiosity, and drive engagement and inquiries." />
 
-        <Card title='Target the Right Audience' description="Forget casting a wide net. We employ advanced targeting strategies to pinpoint homeowners in your local area who are actively seeking remodeling services. By analyzing demographics, interests, and online behavior, we ensure your ads reach the right people at the right time—increasing the likelihood of converting leads into satisfied customers." />
+        <Card title='Target the Right Audience' description="Forget casting a wide net. We employ advanced targeting strategies to pinpoint your ideal customers. By analyzing demographics, interests, and online behavior, we ensure your ads reach the right people at the right time—increasing the likelihood of converting leads into satisfied customers." />
 
-        <Card title='Optimize for Success' description="Success in social media marketing isn't accidental—it's strategic. We meticulously monitor and analyze campaign performance to understand what works best for your remodeling business. With real-time adjustments and continuous optimization, we maximize your return on investment (ROI) by focusing on metrics that matter—whether it's lead generation, website visits, or customer inquiries." />
+        <Card title='Optimize for Success' description="Success in social media marketing isn't accidental—it's strategic. We meticulously monitor." />
       
       </div>
     </section>
