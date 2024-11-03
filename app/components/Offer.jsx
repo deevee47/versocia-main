3592b75b864efe6fdca44fcfeabdf1ddfa0a8cc9
Divyansh Vishwakarma <divyanshvishwakarma4@gmail.com>
2024-11-03 22:34:21 +0530

    winter theme added

--- app/components/Offer.jsx
+++ app/components/Offer.jsx
@@ -15,7 +15,7 @@ const Video = React.forwardRef((props, ref) => {
         ref={ref}
         loading="lazy"
       >
-        <source src="./glass.mp4" type="video/mp4" />
+        <source src="./winter.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
   );
@@ -56,27 +56,43 @@ const Offer = () => {
   }, [observerInstance, videoRef]);
 
   return (
-    <div id="offer" className="w-full flex flex-col sm:py-36 bg-black text-white sm:px-20 pb-0">
-      <Heading label={'The versocia Monsoon Offer'} />
+    <div
+      id="offer"
+      className="w-full flex flex-col sm:py-36 bg-black text-white sm:px-20 pb-0"
+    >
+      <Heading label={"The versocia Monsoon Offer"} />
       <div className="mx-auto sm:mb-36 ">
         <Video ref={videoRef} />
       </div>
 
-      <Heading label={'Adding More to your Glass'} />
-    
+      <Heading label={"Adding More to your Glass"} />
+
       <div className="pt-16 sm:w-[60%] w-[80%] mx-auto flex justify-center flex-col items-center text-left">
         <FadeInUp>
           <div className="pb-16">
-            <span className="font-bold">Monsoon's calling, and so are new opportunities! </span>
-            Partner with Versocia before the season ends and unlock {' '}
-            <span className="font-bold">lifetime FREE premium ad copy insights and strategies valued at $350/month. </span>
+            <span className="font-bold">
+              Winter's calling, and so are new opportunities!{" "}
+            </span>
+            Partner with Versocia before the season ends and unlock{" "}
+            <span className="font-bold">
+              lifetime FREE premium ad copy insights and strategies valued at
+              $350/month.{" "}
+            </span>
           </div>
         </FadeInUp>
 
         <div>
           <FadeInUp>
-            <span className="font-bold">We're not afraid to put our money where our mouth is. </span>
-            That's why we offer a <span className="font-bold">30-day</span>  free trial. If your customer inquiries aren't trending upwards after 30 days, you can keep all the clients we bring you for <span className='font-bold'>completely free</span>, and we won't charge you a thing if you're not satisfied with the results. Think of it as our way of saying, "Let's make this Monsoon outstanding for your business!"
+            <span className="font-bold">
+              We're not just about talk; we&apos;re ready to back it up.{" "}
+            </span>
+            That's why we offer a <span className="font-bold">30-day</span> free
+            trial. If your customer inquiries aren't trending upwards after 30
+            days, you can keep all the clients we bring you for{" "}
+            <span className="font-bold">completely free</span>, and we won't
+            charge you a thing if you're not satisfied with the results. Think
+            of it as our way of saying, "Let's make this Winter outstanding for
+            your business!"
             <br />
           </FadeInUp>
         </div>
